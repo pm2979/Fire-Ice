0b3265414c92723a186678c00fb08eacf370a9f8
miju99 <mji0@naver.com>
2025-05-09 16:55:56 +0900

    feat : Ability.cs, Switch.cs
    각 캐릭터 별 얼음 스위치 충돌 반응 추가

--- FireIce/Assets/01.Scripts/Obstacle/Switch.cs
+++ FireIce/Assets/01.Scripts/Obstacle/Switch.cs
@@ -1,20 +1,32 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Switch : MonoBehaviour
 {
     [SerializeField] private GameObject ObstacleObj;
     [SerializeField] private Animator animator;
+    //[SerializeField] private GameObject IceState; //자식 오브젝트 Ice
+
+    //bool값을 이용해 얼음이 있는 지 없는 지 판단
+    public bool isFrozen { get; set; } //얼음이 붙어있는 지 확인
 
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision) //얼음이 밟았을 때
     {
-        ObstacleObj.GetComponent<IObstacleActive>().IsActive = true;
-        animator.SetBool("IsOn", true);
+        if (isFrozen == true)
+        {
+            ObstacleObj.GetComponent<IObstacleActive>().IsActive = true;
+            animator.SetBool("IsOn", true);
+        }
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
+    private void OnCollisionExit2D(Collision2D collision) //불이 밟았을 때
     {
-        ObstacleObj.GetComponent<IObstacleActive>().IsActive = false;
-        animator.SetBool("IsOn", false);
+        if (isFrozen == false)
+        {
+            ObstacleObj.GetComponent<IObstacleActive>().IsActive = false;
+            animator.SetBool("IsOn", false);
+        }
     }
 }
+
