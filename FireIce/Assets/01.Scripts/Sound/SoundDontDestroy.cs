a5f137c2e3e4c95e102553107e255f2460f7084e
miju99 <mji0@naver.com>
2025-05-14 15:59:40 +0900

    feat : SoundDontDestroy.cs
    - 사운드 UI가 씬 이동에도 삭제 되지 않도록 SoundDontDerstroy.cs 추가
    - Title Scene에 프리팹 추가
    1. VolumeSetting
    2. Volume
    3. SoundSetting

---
+++ FireIce/Assets/01.Scripts/Sound/SoundDontDestroy.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundDontDestroy : Singleton<SoundDontDestroy>
+{
+    protected override void Awake()
+    { 
+        base.Awake();
+    }
+}
