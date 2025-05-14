fc270e7f0da23d9df8f3b3064e9178477de03680
miju99 <mji0@naver.com>
2025-05-14 12:30:46 +0900

    feat : Sound UI
    사운드 UI 프리팹
    - SoundSetting
    - Volume
    - VolumeSetting
    사운드 스크립트
    - VolumeVontroller.cs 수정
    TestScene에서 테스트 해봄.

---
+++ FireIce/Assets/01.Scripts/Sound/VolumeController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeController : Singleton<VolumeController>
+{
+    [SerializeField] private GameObject volumePanel;
+    [SerializeField] private Button volumeButton;
+    [SerializeField] private Button backgroundBlockButton;
+
+    private bool inVisible = false;
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    private void Start()
+    {
+        volumePanel.SetActive(false);
+        volumeButton.onClick.AddListener(ToggleVolumePanel);
+        backgroundBlockButton.onClick.AddListener(ClosePanel);
+    }
+
+    private void ToggleVolumePanel()
+    {
+        inVisible = !inVisible;
+        volumePanel.SetActive(inVisible);
+    }
+
+    private void ClosePanel()
+    {
+        inVisible = false;
+        volumePanel.SetActive(false);
+    }
+}
