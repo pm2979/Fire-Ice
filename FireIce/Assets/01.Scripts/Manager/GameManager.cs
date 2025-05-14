13248cb7a1c770ac0df762ebd3f091a5f3065f4b
pm2979 <pm29798512@gmail.com>
2025-05-14 10:18:56 +0900

    Comment : 주석 추가
    Comment : 주석 추가

--- FireIce/Assets/01.Scripts/Manager/GameManager.cs
+++ FireIce/Assets/01.Scripts/Manager/GameManager.cs
@@ -8,6 +8,7 @@ public class GameManager : Singleton<GameManager>
     private StageUIController uIController;
     private int totalDoor;
     private int openDoor;
+
     protected override void Awake()
     {
         base.Awake();
@@ -22,6 +23,7 @@ public class GameManager : Singleton<GameManager>
         openDoor = 0;
         uIController = FindObjectOfType<StageUIController>();
     }
+
     private void Update()
     {
         if(uIController != null)
@@ -29,6 +31,7 @@ public class GameManager : Singleton<GameManager>
             ESC();
         }
     }
+
     private void ESC()
     {
         if(Input.GetKeyDown(KeyCode.Escape))
@@ -37,6 +40,7 @@ public class GameManager : Singleton<GameManager>
             Debug.Log("Esc Å° ´©¸§");
         }
     }
+
     public void GameOverUI()
     {
         if(uIController != null)
@@ -56,6 +60,4 @@ public class GameManager : Singleton<GameManager>
                 scoreMg.Rank();
         }
     }
-
-
 }
