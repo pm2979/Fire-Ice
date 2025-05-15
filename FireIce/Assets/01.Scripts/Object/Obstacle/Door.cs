ac29a033b86d2cf55fc6b2fc29d15572a88fd078
pm2979 <pm29798512@gmail.com>
2025-05-15 10:38:57 +0900

    Add : 빌드 파일 추가
    빌드 파일 추가

--- FireIce/Assets/01.Scripts/Object/Obstacle/Door.cs
+++ FireIce/Assets/01.Scripts/Object/Obstacle/Door.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
-using static UnityEditor.PlayerSettings;
-
 
 public class Door : MonoBehaviour, IObstacleActive
 {
